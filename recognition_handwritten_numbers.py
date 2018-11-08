a4acb2a401947ce7ca454b710bb0d2d3ca6b58a3
Mykhailo Mykytyn <mykhailo.m@zazmic.com>
2018-11-08 15:11:45 +0200

    save model and weights to files

--- recognition_handwritten_numbers.py
+++ recognition_handwritten_numbers.py
@@ -10,13 +10,11 @@ np.random.seed(42)
 # load data
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-
 #  transformation data
 X_train = X_train.reshape(60000, 784)
 # normalisation data
 X_train = X_train.astype('float32')
 X_train /= 255
-
 
 # transformation labels to categories
 y_train = np_utils.to_categorical(y_train, 10)
@@ -28,6 +26,11 @@ model = Sequential()
 model.add(Dense(800, input_dim=784, kernel_initializer='normal', activation='relu'))
 model.add(Dense(10, kernel_initializer='normal', activation='softmax'))
 
+# save model into file
+# model_json = model.to_json()
+# with open('recognition_handwritten_numbers.json', 'w') as f:
+#     f.write(model_json)
+
 # compile model
 model.compile(loss='categorical_crossentropy', optimizer="SGD", metrics=["accuracy"])
 print(model.summary())
@@ -35,6 +38,10 @@ print(model.summary())
 # network training
 # validation_split - 20% of data is test sample
 model.fit(X_train, y_train, batch_size=200, nb_epoch=100, validation_split=0.2, verbose=1)
+
+# save weights
+# model.save_weights('recognition_handwritten_numbers.h5')
+
 
 # transformation output from category to class label(digits from 0 to 9)
 predictions = model.predict(X_train)
